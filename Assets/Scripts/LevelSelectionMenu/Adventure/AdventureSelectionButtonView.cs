e5e23e7406d364a74956ebbee34e44f03c38d8f9
wizard2122 <jackovlev.ilia2018@yandex.ru>
2022-09-01 14:01:04 +0300

    View button clear

--- Assets/Scripts/LevelSelectionMenu/Adventure/AdventureSelectionButtonView.cs
+++ Assets/Scripts/LevelSelectionMenu/Adventure/AdventureSelectionButtonView.cs
@@ -6,18 +6,13 @@ using System;
 public class AdventureSelectionButtonView : SimpleButtonView
 {
     [SerializeField] private TMP_Text _percantageCompletion;
-    [SerializeField] private RectTransform _button;
 
     private const string TitleTemplate = "Путешествие {0}";
     private const string PercantageCompletionTemplate = "{0:f0}%";
 
-    private Vector2 _showPosition => _button.anchoredPosition;
-    private Vector2 HidePosition => _showPosition + Vector2.right * 50;
-
     public void Initialize(int adventureNumber, float percantageCompletion)
     {
         Initialize(string.Format(TitleTemplate, adventureNumber));
-        _button = GetComponent<RectTransform>();
         _percantageCompletion.text = string.Format(PercantageCompletionTemplate, percantageCompletion);
     }
 }
